3b5fe624d50c2a17e755aebebac6f593b4799f96
Md Salauddin Sakib <salauddinsakib99@gmail.com>
2025-02-18 10:50:34 +0600

    create a footer with nayem vai

--- footer.php
+++ footer.php
@@ -1,4 +1,46 @@
 </div>
+
+
+
+<div class="footer-area">
+    <div class="container">
+        <div class="footer-wrapper">
+            <div class="single-footer">
+                <h3>About Us</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt corporis tenetur delectus accusantium modi unde nesciunt ad quibusdam libero tempora.</p>
+            </div>
+            <div class="single-footer">
+                <h3>Our Social Media</h3>
+                <ul>
+                    <li><a href="#">Linkin</a></li>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Instagram</a></li>
+                </ul>
+            </div>
+            <div class="single-footer">
+                <h3>Our Policies</h3>
+                <ul>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Refund Policy</a></li>
+                    <li><a href="#">Return Policy</a></li>
+                    <li><a href="#">Exchange Policy</a></li>
+                    <li><a href="#">Terms and Conditions</a></li>
+                </ul>
+            </div>
+            <div class="single-footer">
+                <h3>Our Important Pages</h3>
+                <ul>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Career</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Support</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="footer">
     &copy; Copyright <?php echo date('Y') ?>
 </div>
